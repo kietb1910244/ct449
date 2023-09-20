e83c31d359a8b71a5b246223338822a8f6edcc2a
kietb1910244 <kietb1910244@student.ctu.edu.vn>
2023-09-20 10:31:41 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+module.exports = app;
 
 app.get("/", (req, res) => {
   res.json({ message: "Wwlcome to contact book application." });
